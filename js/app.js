3fb5705c1b6c02cb776036b13c0484e8f0e813bd
GabrielOrozco13 <frankoabraham045@gmail.com>
2022-08-19 10:47:43 -0600

    4

--- js/app.js
+++ js/app.js
@@ -4,12 +4,6 @@ var swLocation = '/Twittor/sw.js';
 
 if ( navigator.serviceWorker ) {
 
-
-    if ( url.includes('localhost') ) {
-        swLocation = '/sw.js';
-    }
-
-
     navigator.serviceWorker.register( swLocation );
 }
 
