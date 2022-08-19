65270f56702469c83806b2aefc64b4d3b73f058e
GabrielOrozco13 <frankoabraham045@gmail.com>
2022-08-19 11:56:12 -0600

    Mods

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = "static-v1";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v2";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 
@@ -59,6 +59,9 @@ self.addEventListener('activate', e => {
                     return caches.delete(key);
                 }
 
+                if (key !== DYNAMIC_CACHE_CACHE && key.includes('dynamic')) {
+                    return caches.delete(key);
+                }
             });
 
         });
